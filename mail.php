2dc0723ea184ff423121e6f27d020aee6b20024c
Marcin Chojnacki <marcinchojnacki00@gmail.com>
2017-08-11 20:34:55 +0200

    v0.4

--- mail.php
+++ mail.php
@@ -1,78 +1,41 @@
 <?php
 
-$fnameError = $lnameError = $emailError = "";
 
-$fname = $_POST['fname'] . "\r\n";
-$lname = $_POST['lname'] . "\r\n";
-$email = $_POST['email'] . "\r\n";
+$fname = trim($_POST['fname']) . "\r\n";
+$lname = trim($_POST['lname']) . "\r\n";
+$email = trim($_POST['email']) . "\r\n";
 
-$fnamee = 'Imie: ' . $_POST['fname'];
-$lnamee = 'Nazwisko: ' . $_POST['lname'];
-$address = 'Adres: ' . $_POST['address'];
-$postal = 'Kod pocztowy: ' . $_POST['postal'];
-$phone = 'Numer telefonu: ' . $_POST['phone'];
-$emaill = 'Adres email: ' . $_POST['email'];
-$date = 'Termin: ' . $_POST['date'];
-$topic = 'Temat: ' . $_POST['topic'];
-$message = 'Wiadomość: ' . $_POST['textarea'];
+$address = trim($_POST['address']);
+$postal = trim($_POST['postal']);
+$phone = trim($_POST['phone']);
+$date = trim($_POST['date']);
+$topic = trim($_POST['topic']);
+$message = trim($_POST['textarea']);
 
-$msg = "$fnamee
-";
-$msg.= "$lnamee
-";
-$msg.= "$address
-";
-$msg.= "$postal
-";
-$msg.= "$phone
-";
-$msg.= "$emaill
-";
-$msg.= "$date
-";
-$msg.= "$topic
-";
-$msg.= "$message
-";
+$msg = "Imię: $fname\n";
+$msg .= "Nazwisko: $lname\n";
+$msg .= "Adres: $address\n";
+$msg .= "Kod pocztowy: $postal\n";
+$msg .= "Numer telefonu: $phone\n";
+$msg .= "Adres e-mail: $email\n";
+$msg .= "Termin: $date\n";
+$msg .= "Temat: $topic\n";
+$msg .= "Wiadomość:\n $message\n";
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    
-    if (empty($_POST['fname'])) {
-        $fnameError = "error";
-    } else {
-        $fname = test_input($_POST["fname"]);
-    }
-    if (empty($_POST['lname'])) {
-        $lnameError = "error";
-    } else {
-        $lname = test_input($_POST["lname"]);
-    }
-    if (empty($_POST['email'])) {
-        $emailError = "error";
-    } else {
-        $email = test_input($_POST["email"]);
-    }
-    
-    if($fnameError == "" and $lnameError == "" and $emailError == "") {
-        
-        $flname = "$fname
-        ";
-        $flname.= "$lname
-        ";
-        $to = 'marcinchojnacki00@gmail.com';
-        $subject = 'Wiadomośc z formularza!';
-        mail($to, $flname, $email, $subject, $msg);
-//        if(mail($to, $flname, $email, $subject, $msg)){
-//            $success = "Formularz wysłany, dziekujemy za wiadomość!";
-//        }
-    }
-}
 
-function test_input($data) {
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  return $data;
-}
+
+$to = 'marcinchojnacki00@gmail.com';
+$subject = 'Wiadomośc z formularza od $fname $lname!';
+$headers = "From: $fname $lname <$email>";
+
+mail($to, $subject, $msg, $headers);
+
+
+//function test_input($data) {
+//  $data = trim($data);
+//  $data = stripslashes($data);
+//  $data = htmlspecialchars($data);
+//  return $data;
+//}
 
 ?>
